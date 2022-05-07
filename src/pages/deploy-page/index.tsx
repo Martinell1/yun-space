cf74490a2b3dcc9d65b037ca101be45d46c2107c
AsheOne <1029129827@qq.com>
2022-05-07 22:23:51 +0800

    路由配置和组件导入

--- src/pages/deploy-page/index.tsx
+++ src/pages/deploy-page/index.tsx
@@ -1,12 +1,34 @@
-import { Form, Input, Button, Radio } from 'antd';
+import { Form, Input } from 'antd';
+import { useEffect, useState } from 'react';
 export default function DeployPage(){
+    const [form,setForm] = useState({
+        accessKey:'',
+        secretKey:'',
+        bucket:'',
+    })
+    
+    useEffect(()=>{
+        setForm({
+            accessKey:'GHtgzdXyBwen6ktRdnM3TruspfORzrqpJcQdOnOj',
+            secretKey:'ljJdPY5eSiV6nWwNkEgsNnhBqNZ3J3DqDOk8drGS',
+            bucket:'light-hub',
+        })
+    },[])
     return (
-        <Form>
-            <Form.Item label="Field A">
-                <Input placeholder="input placeholder" />
+        <Form
+            style={{padding:'20px'}}
+            colon={false}
+            labelCol={{ span: 1 }}
+            initialValues={form}
+            >
+            <Form.Item label="accessKey" name='accessKey'>
+                <Input  placeholder="请输入accessKey"/>
             </Form.Item>
-            <Form.Item label="Field B">
-                <Input placeholder="input placeholder" />
+            <Form.Item label="secretKey" name='secretKey'>
+                <Input placeholder="请输入secretKey"/>
+            </Form.Item>
+            <Form.Item label="bucket" name='bucket'>
+                <Input placeholder="请输入bucket"/>
             </Form.Item>
         </Form>
     )
--- src/pages/manage-page/index.tsx
+++ src/pages/manage-page/index.tsx
@@ -1,7 +1,9 @@
+
+import './index.css'
 export default function ManagePage(){
     return (
         <div>
-            管理
+
         </div>
     )
 }
