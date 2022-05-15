680884f440b660a40283fa4ff1ba7dcb34e4d3af
AsheOne <1029129827@qq.com>
2022-05-15 20:26:11 +0800

    图床管理显示选中文件夹图片

--- src/pages/manage-page/index.tsx
+++ src/pages/manage-page/index.tsx
@@ -4,30 +4,34 @@ import { Image } from 'antd';
 import './index.css'
 
 export default function ManagePage(){
-    const {management} = useAppContext()
-    const [visible, setVisible] = useState(false);
+    const {config,management} = useAppContext()
+    const [currentDir,setCurrentDir] = useState(config.dir)
     return (
-        <div></div>
-        // <div style={{padding:'20px 0'}}>
-        // <Image
-        //   style={{objectFit:'cover'}}
-        //   src={management[management.length-1].url}
-        //   preview={{ visible: false }}
-        //   width={200}
-        //   height={140}
-        //   onClick={() => setVisible(true)}
-        // />
-        // <div style={{ display: 'none' }}>
-        //   <Image.PreviewGroup preview={{ visible, onVisibleChange: vis => setVisible(vis) }}>
-        //     {
-        //         management.map((item)=>{
-        //             return (
-        //                 <Image key={item.name} src={item.url} />
-        //             )
-        //         })
-        //     }
-        //   </Image.PreviewGroup>
-        // </div>
-        // </div>
+        <div style={{padding:'20px 0'}}>
+        {
+            management.map((dir,index) => {
+                return (
+                    <div>
+                        {
+                            dir.dir === currentDir ?
+                            dir.imageList.map((image)=>{
+                                return (
+                                    <Image
+                                        style={{objectFit:'cover'}}
+                                        src={image.url}
+                                        width={200}
+                                        height={140}
+                                        onClick={() => {
+                                        }}
+                                    /> 
+                                )
+                            })
+                            : ''
+                        }
+                    </div>
+                )
+            })
+        }
+        </div>
     )
 }
