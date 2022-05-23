8a916b3d09cc99b636665af0b74b9512b345a6ab
AsheOne <1029129827@qq.com>
2022-05-23 17:51:53 +0800

    删除图片

--- src/components/image-expand/index.tsx
+++ src/components/image-expand/index.tsx
@@ -1,6 +1,6 @@
-import { Button, Input, message } from "antd"
+import { Button, Input, message, Popconfirm } from "antd"
 import { imageProps, useAppContext } from "../../store"
-import { MediumOutlined,CopyOutlined } from '@ant-design/icons';
+import { MediumOutlined,CopyOutlined,DeleteOutlined,QuestionCircleOutlined } from '@ant-design/icons';
 import { useState } from "react";
 const ImageExpand = (props:imageProps)=>{
     const [isMarkDown,SetIsMarkDown] = useState(false)
@@ -27,26 +27,57 @@ const ImageExpand = (props:imageProps)=>{
             </Input>
             <div
                 style={{padding:'4px 11px 0 11px',display:'flex',justifyContent:'space-between'}}>
-                <Button 
-                    type={isMarkDown ? 'primary' : 'default'}
-                    size="small" 
-                    icon={<MediumOutlined />} 
-                    onClick={()=>{
-                        SetIsMarkDown(!isMarkDown)
-                    }}/>
-                <Button 
-                    size="small" 
-                    icon={<CopyOutlined />}
-                    onClick={() => {
-                        if(isMarkDown){
-                            clipboardObj.writeText(`![${props.name}](${props.url})`);
-                        }else{
-                            clipboardObj.writeText(props.url);
-                        }
-                       
-                        message.success(`已拷贝${isMarkDown ? 'Markdown' : ''}链接`)
+                <div>
+                    <Button 
+                        type={isMarkDown ? 'primary' : 'default'}
+                        size="small" 
+                        icon={<MediumOutlined />} 
+                        onClick={()=>{
+                            SetIsMarkDown(!isMarkDown)
+                        }}/>
+                    <Button 
+                        size="small" 
+                        icon={<CopyOutlined />}
+                        onClick={() => {
+                            if(isMarkDown){
+                                clipboardObj.writeText(`![${props.name}](${props.url})`);
+                            }else{
+                                clipboardObj.writeText(props.url);
+                            }
+                        
+                            message.success(`已拷贝${isMarkDown ? 'Markdown' : ''}链接`)
+                        }}
+                    />
+                </div>
+                <Popconfirm 
+                    title="Are you sure?" 
+                    icon={<QuestionCircleOutlined 
+                        style={{ color: 'red' }}/>
+                    }
+                    onConfirm ={()=>{
+                        management.map(dir => {
+                            let index = -1
+                            if(dir.dir === props.dir){
+                                for(let i = 0 ; i < dir.imageList.length; i++){
+                                    if(dir.imageList[i].id === props.id){
+                                        index = i
+                                        break
+                                    }
+                                }
+                                dir.imageList.splice(index,1)
+                            }
+                            return dir
+                          })
+                          setManagement(management)
+                          message.success('删除成功')
                     }}
-                />
+                >
+                    <Button 
+                        size="small" 
+                        icon={<DeleteOutlined />}
+                    />
+                </Popconfirm>
+
             </div>
         </div>
     )
